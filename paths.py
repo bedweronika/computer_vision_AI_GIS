6a68715b681652417128415a72e44c48bf41711f
Nika <bedweronika@gmail.com>
2026-02-08 23:51:15 +0100

    add path for cross-points files

--- paths.py
+++ paths.py
@@ -5,3 +5,6 @@ files_folder="./files/"
 raw_maps=f"{files_folder}raw_maps/"    # folder with images with maps
 scalled_maps=f"{files_folder}scalled_maps/"   # folder with images with maps in local scale
 proccesed_maps=f"{files_folder}proccesed_maps/"    # folder with images with maps after AI proccessing
+
+# csv files
+raw_map_cross_points = f"{files_folder}raw_map_cross_points/" 
