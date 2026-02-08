f98c1e1361facce012e1da4517e518f50b94dc50
Nika <bedweronika@gmail.com>
2026-02-08 20:16:24 +0100

    initial local repo to remote

---
+++ paths.py
@@ -0,0 +1,7 @@
+#path to folder with files
+files_folder="./files" 
+
+#paths to folder with maps
+raw_maps=f"{files_folder}/raw_maps"    # folder with images with maps
+scalled_maps=f"{files_folder}/scalled_maps"   # folder with images with maps in local scale
+proccesed_maps=f"{files_folder}/proccesed_maps"    # folder with images with maps after AI proccessing
