0ce064b04e6a8b6ed06cbda97aa241aa1ca42931
Nika <bedweronika@gmail.com>
2026-02-09 23:02:08 +0100

    initial jupyter notebook for analysis and add maps_for_processing foleder to paths

--- paths.py
+++ paths.py
@@ -4,6 +4,7 @@ files_folder="./files/"
 #paths to folder with maps
 raw_maps=f"{files_folder}raw_maps/"    # folder with images with maps
 scalled_maps=f"{files_folder}scalled_maps/"   # folder with images with maps in local scale
+maps_for_processing =f"{files_folder}maps_for_processing"   # folder with maps choosen the best from all maps -> the base AI
 proccesed_maps=f"{files_folder}proccesed_maps/"    # folder with images with maps after AI proccessing
 
 # csv files
