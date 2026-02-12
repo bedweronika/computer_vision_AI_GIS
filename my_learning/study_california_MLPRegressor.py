25577e950adc0e0e861f03367ca4c2c868795dcd
Nika <bedweronika@gmail.com>
2026-02-13 00:27:59 +0100

    add comments MLPRegressor

--- my_learning/study_california_MLPRegressor.py
+++ my_learning/study_california_MLPRegressor.py
@@ -9,16 +9,21 @@ from sklearn.preprocessing import StandardScaler
 
 """
 print("Start script")
+"fetch data"
 housing = fetch_california_housing()
+print(housing.data.shape, housing.target.shape)
+print(housing.feature_names[0:6])
+
+"split data"
 X_train_full, X_test, y_train_full, y_test = train_test_split(housing.data, housing.target, random_state=42)
 X_train, X_valid, y_train, y_valid = train_test_split(X_train_full, y_train_full, random_state=42)
 
+"pipeline to standarize the input"
 mlp_reg = MLPRegressor(hidden_layer_sizes=[50, 50, 50], random_state=42)
 pipeline = make_pipeline(StandardScaler(), mlp_reg)
-pipeline.fit(X_train, y_train)
+pipeline.fit(X_train, y_train)  # trained uding gradiend descent, does not coverage when the features have very different scales
 y_pred=pipeline.predict(X_valid)
-rmse = root_mean_squared_error(y_valid, y_pred) # about 0.505
+rmse = root_mean_squared_error(y_valid, y_pred) # result is about 0.505
 
-print(rmse)
 
 print("End script")
