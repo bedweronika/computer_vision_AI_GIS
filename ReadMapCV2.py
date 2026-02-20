a54b00455df7896279cee503d60ac8e6bce0fea5
Nika <bedweronika@gmail.com>
2026-02-21 00:13:50 +0100

    Cut the file to smaller tiles

--- ReadMapCV2.py
+++ ReadMapCV2.py
@@ -8,20 +8,20 @@ class ReadMapCV2:
     def __init__(self, file_path, is_gray: bool = True):
         self.file_path = file_path
         self.is_gray = is_gray
-        self._img = None
+        self.loaded_img = None
 
     @property
     def img(self):
-        if self._img is None:
+        if self.loaded_img is None:
             if self.is_gray:
-                self._img = cv2.imread(self.file_path, cv2.IMREAD_GRAYSCALE)
+                self.loaded_img = cv2.imread(self.file_path, cv2.IMREAD_GRAYSCALE)
             else :
-                self._img = cv2.imread(self.file_path)
+                self.loaded_img = cv2.imread(self.file_path)
 
-            if self._img is None:
+            if self.loaded_img is None:
                 raise ValueError("The image cannot be loaded: ", self.file_path)
             
-            if len(self._img.shape)>2 and self.is_gray:
-                raise ValueError("The image should be in gray scale, image shape: ", self._img.shape)
+            if len(self.loaded_img.shape)>2 and self.is_gray:
+                raise ValueError("The image should be in gray scale, image shape: ", self.loaded_img.shape)
 
-        return self._img
+        return self.loaded_img
